dd738b9b7ee0a0906840b59fdf8b91d22b391184
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2022-03-01 06:19:21 +0630

    update

--- routes/gogo-route.ts
+++ routes/gogo-route.ts
@@ -1,19 +1,35 @@
 import express from "express";
 
 import { getEpisodeByLink, getRecentReleases } from "../scrapper/gogo";
+import { getRedisClient } from "../utils/redis";
 
 const router = express.Router();
 
 router.get("/recent-releases", async (req, res) => {
   try {
+    const client = await getRedisClient();
+    const value = await client.get(
+      `fetchit:recent-releases:${JSON.stringify(req.query)}`
+    );
+    if (value) {
+      res.json(JSON.parse(value));
+    }
+
     const p: number = parseInt((req.query.page as string) || "1");
     const data = await getRecentReleases(p);
-    res.json({
+    const response = {
       code: 200,
       message: "Success",
       data: data.animes,
       page: data.page,
-    });
+    };
+    if (!value) {
+      res.json(response);
+    }
+    client.set(
+      `fetchit:recent-releases:${JSON.stringify(req.query)}`,
+      JSON.stringify(response)
+    );
   } catch (err) {
     console.log(err);
     res.status(500).json({ code: 500, message: "Server Error!" });
