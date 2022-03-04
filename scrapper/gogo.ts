84f324174ec5afb7cd5beac852ad763e02244e78
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2022-03-04 06:51:51 +0630

    update

--- scrapper/gogo.ts
+++ scrapper/gogo.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 
-const host = "https://ww2.gogoanimes.org";
+const host = "https://gogoanime.fi";
+const ajax = "https://ajax.gogo-load.com";
 
 export const getAnimes = async (m: string, p: number = 1) => {
-  const { data } = await axios.get(`${host}/${m}?page=${p}`);
+  const { data } = await axios.get(
+    `${host}/${m}${host.includes("fi") ? ".html" : ""}?page=${p}`
+  );
   const $ = cheerio.load(data);
   const anchors: any[] = [];
   $("ul.items p.name a").each(function (i, el) {
@@ -43,18 +46,31 @@ export const getAnimeInfo = async (c: string) => {
   const infos: any = {};
   $(".anime_info_body_bg p").each(function (i, el) {
     const [k, v] = $(this).text().trim().split(":");
-    if (k.toLowerCase() == "genre") {
-      infos[k] = v.split(",").map((x) => x.trim());
-    } else {
-      infos[k] = v.replaceAll("\n", "").replaceAll("\t", "").trim();
+    if (k != "Click to manage book marks") {
+      if (k.toLowerCase() == "genre") {
+        infos[k] = v.split(",").map((x) => x.trim());
+      } else {
+        infos[k] = v ? v.replaceAll("\n", "").replaceAll("\t", "").trim() : "";
+      }
     }
   });
 
   const eps: any[] = [];
   $("#episode_page li a").each(function (i, el) {
+    let movie_id = "";
+    let default_ep = "";
+    let alias = "";
+    if (host.includes(".fi")) {
+      movie_id = $("#movie_id").attr("value") || "";
+      default_ep = $("#default_ep").attr("value") || "";
+      alias = $("#alias_anime").attr("value") || "";
+    }
     eps.unshift({
       ep_start: $(this).attr("ep_start"),
       ep_end: $(this).attr("ep_end") || $(this).attr("ep_start"),
+      movie_id,
+      default_ep,
+      alias,
     });
   });
 
@@ -62,9 +78,19 @@ export const getAnimeInfo = async (c: string) => {
 };
 
 export const getRecentReleases = async (p: number = 1) => {
-  const { data } = await axios.get(
-    `${host}/ajax/page-recent-release?page=${p}&type=1`
-  );
+  let data = null;
+  if (host.includes(".fi")) {
+    const res = await axios.get(
+      `${ajax}/ajax/page-recent-release.html?page=${p}&type=1`
+    );
+    data = res.data;
+  } else {
+    const res = await axios.get(
+      `${host}/ajax/page-recent-release?page=${p}&type=1`
+    );
+    data = res.data;
+  }
+
   const $ = cheerio.load(data);
   const anchors: any[] = [];
   $("ul.items p.name a").each(function (i, el) {
@@ -99,10 +125,15 @@ export const getRelatedEpisodes = async (c: string, eps: any[] = []) => {
   if (c.includes("-episode")) {
     alias = `/category/${c.split("/").pop()?.split("-episode")[0]}`;
   }
+
   const related_episodes: any = [];
   for (const ep of eps) {
     const epres = await axios.get(
-      `${host}/ajaxajax/load-list-episode?ep_start=${ep.ep_start}&ep_end=${ep.ep_end}&id=0&default_ep=&alias=${alias}`
+      `${host.includes(".fi") ? ajax : host}/ajax/load-list-episode?ep_start=${
+        ep.ep_start
+      }&ep_end=${ep.ep_end}&id=${ep.movie_id || "0"}&default_ep=${
+        ep.default_ep || ""
+      }&alias=${host.includes(".fi") ? ep.alias : alias}`
     );
     const $ = cheerio.load(epres.data);
     $("#episode_related a").each(function (i, el) {
@@ -116,16 +147,30 @@ export const getRelatedEpisodes = async (c: string, eps: any[] = []) => {
 };
 
 export const getEpisodeByLink = async (link: string) => {
-  const { data } = await axios.get(link);
+  const { data } = await axios.get(`${host}/${link}`);
   let $ = cheerio.load(data);
-  const stream = $("#load_anime iframe").attr("src");
-  const title = $(".anime_video_body h1").text();
+  let stream = $("#load_anime iframe").attr("src");
+
+  const title = $(".anime_video_body h1")
+    .text()
+    .replace("English Subbed at gogoanime", "");
 
   const eps: any[] = [];
   $("#episode_page li a").each(function (i, el) {
+    let movie_id = "";
+    let default_ep = "";
+    let alias = "";
+    if (host.includes(".fi")) {
+      movie_id = $("#movie_id").attr("value") || "";
+      default_ep = $("#default_ep").attr("value") || "";
+      alias = $("#alias_anime").attr("value") || "";
+    }
     eps.unshift({
       ep_start: $(this).attr("ep_start"),
       ep_end: $(this).attr("ep_end") || $(this).attr("ep_start"),
+      movie_id,
+      default_ep,
+      alias,
     });
   });
 
@@ -134,7 +179,9 @@ export const getEpisodeByLink = async (link: string) => {
 
 export const searchAnimes = async (keyword: string, p: number = 1) => {
   const { data } = await axios.get(
-    `${host}/search?keyword=${keyword}&page=${p}`
+    `${host}/search${
+      host.includes(".fi") ? ".html" : ""
+    }?keyword=${keyword}&page=${p}`
   );
   const $ = cheerio.load(data);
   const anchors: any[] = [];
